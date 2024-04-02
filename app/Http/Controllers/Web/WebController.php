6c0baae4e1810e221a1286e4977c7940b67f78a2
Arifur Rahman <arifur.rahman@bd-dac-co.giz.de>
2024-04-02 11:09:43 +0600

    test

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -17,23 +17,23 @@ class WebController extends Controller
         $send['provider_ntcs'] = DB::table('events')
                 ->join('event_types', 'events.event_type_id', '=', 'event_types.id')
                 ->select('events.event_title','events.event_type_id', 'events.url', 'events.start_date', 'event_types.type_name', 'events.created_at', 'events.upload')
-                ->where(['events.event_status' => 1])
+                ->where(['events.event_status' => 1, 'events.dept_id' => 0])
                 // ->where('events.end_date', '>=', now())
                 ->orderByDesc('events.end_date')
                 ->get();
 
 
-        $send['histories'] = DB::table('histories')->first();
-        $send['messages'] = DB::table('messages')->orderByDesc('created_at')->first();
-        $send['uploads'] = DB::table('uploads')->where(['status' => 1])
+        $send['histories'] = DB::table('histories')->where('dept_id', 0)->first();
+        $send['messages'] = DB::table('messages')->where('dept_id', 0)->orderByDesc('created_at')->first();
+        $send['uploads'] = DB::table('uploads')->where(['status' => 1, 'dept_id' => 0])
         ->orderByDesc('created_at')
         ->limit(6) // Replace '5' with your desired limit
         ->get();
-        $send['sliders'] = DB::table('sliders')->where(['slider_status' => 1])
+        $send['sliders'] = DB::table('sliders')->where(['slider_status' => 1, 'dept_id' => 0])
         ->limit(4) // Replace '5' with your desired limit
         ->get();
 
-        $events = Events::where('event_status', 1)
+        $events = Events::where(['event_status' => 1, 'dept_id' => 0])
         // ->whereDate('start_date', '>=', now())
         ->select('event_title', 'start_date', 'end_date', 'url', 'color')
         ->get()
@@ -59,7 +59,7 @@ class WebController extends Controller
         $send['events'] = DB::table('events')
         ->select('events.*',  'event_types.type_name')
         ->join('event_types', 'events.event_type_id', '=', 'event_types.id')
-        ->where(['events.event_status' => 1, 'events.url' => $slug])
+        ->where(['events.event_status' => 1, 'events.url' => $slug, 'dept_id' => 0])
         ->first();
         return view('web.notice_detail', $send);
     }
@@ -71,7 +71,7 @@ class WebController extends Controller
 
             $send['childmenudesc'] = DB::table('child_menus')
             ->select('childmenu_name', 'child_menu_slug', 'child_menu_desc', 'upload')
-            ->where(['child_menu_status' => 1, 'child_menu_slug' => $childmenu])
+            ->where(['child_menu_status' => 1, 'child_menu_slug' => $childmenu, 'dept_id' => 0])
             ->first();
             return view('web.childmenudesc', $send);
 
@@ -79,7 +79,7 @@ class WebController extends Controller
 
             $send['submenudesc'] = DB::table('sub_menus')
             ->select('submenu_name', 'submenu_slug', 'submenu_desc', 'upload')
-            ->where(['submenu_status' => 1, 'submenu_slug' => $submenu])
+            ->where(['submenu_status' => 1, 'submenu_slug' => $submenu, 'dept_id' => 0])
             ->first();
 
             return view('web.submenudesc', $send);
@@ -88,7 +88,7 @@ class WebController extends Controller
 
             $send['menudesc'] = DB::table('menus')
             ->select('menu_name', 'menu_slug', 'menu_desc', 'upload')
-            ->where(['menu_status' => 1, 'menu_slug' => $menu])
+            ->where(['menu_status' => 1, 'menu_slug' => $menu, 'dept_id' => 0])
             ->first();
 
             return view('web.menudesc', $send);
@@ -104,11 +104,12 @@ class WebController extends Controller
     {
         $eventType = DB::table('event_types')
         ->where('type_name', $slug)
+        ->where('dept_id', 0)
         ->first();
 
         $send['notices'] = DB::table('events')
         ->join('event_types', 'events.event_type_id', '=', 'event_types.id')
-        ->where(['events.event_type_id' => $eventType->id, 'events.event_status' => 1])
+        ->where(['events.event_type_id' => $eventType->id, 'events.event_status' => 1, 'events.dept_id' => 0])
         ->orderByDesc('events.start_date')
         // ->limit(6) // Replace '5' with your desired limit
         ->get(['event_types.type_name', 'events.*']);
@@ -119,13 +120,13 @@ class WebController extends Controller
 
     public function history_details()
     {
-        $send['histories'] = DB::table('histories')->first();
+        $send['histories'] = DB::table('histories')->where('dept_id', 0)->first();
         return view('web.historydetails', $send);
     }
 
     public function mujib_corner()
     {
-        $send['mujibs'] = DB::table('mujib_corners')->where('status', 1)->get();
+        $send['mujibs'] = DB::table('mujib_corners')->where(['status' => 1, 'dept_id' => 0])->get();
         return view('web.mujib-corner', $send);
     }
 
@@ -137,13 +138,13 @@ class WebController extends Controller
 
     public function message($slug=null)
     {
-        $send['msgs'] = DB::table('messages')->where(['message_status' => 1, 'message_slug'=>$slug])->first();
+        $send['msgs'] = DB::table('messages')->where(['message_status' => 1, 'message_slug'=>$slug, 'dept_id' => 0])->first();
         return view('web.msgs', $send);
     }
 
     public function alldownload()
     {
-        $send['uploads'] = DB::table('uploads')->where(['status' => 1])
+        $send['uploads'] = DB::table('uploads')->where(['status' => 1, 'dept_id' => 0])
         ->orderByDesc('created_at')
         ->get();
         return view('web.alldownload' , $send);
@@ -151,7 +152,7 @@ class WebController extends Controller
 
     public function download($slug=null)
     {
-        $send['upload'] = DB::table('uploads')->where(['status' => 1, 'url'=>$slug])->first();
+        $send['upload'] = DB::table('uploads')->where(['status' => 1, 'url'=>$slug, 'dept_id' => 0])->first();
         return view('web.download', $send);
     }
 
--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -33,9 +33,9 @@ class ViewServiceProvider extends ServiceProvider
                 'subMenus.childMenus' => function ($query) {
                     $query->where('child_menu_status', 1); // Filter child_menus by child_menu_status
                 },
-            ])->where('menu_status', 1)->get();
+            ])->where(['menu_status' => 1, 'dept_id' => 0])->get();
 
-            $send['important_links'] = DB::table('important_links')->select('link_name', 'link')->get();
+            $send['important_links'] = DB::table('important_links')->select('link_name', 'link')->where(['dept_id' => 0])->get();
 
             // $send['sliders'] = DB::table('sliders')->where('slider_status', 1)->get();
 
@@ -47,6 +47,7 @@ class ViewServiceProvider extends ServiceProvider
                 ->join('event_types', 'events.event_type_id', '=', 'event_types.id')
                 ->select('events.event_title', 'events.event_type_id', 'events.url', 'events.start_date', 'events.end_date', 'event_types.type_name', 'events.created_at', 'events.upload')
                 ->where('events.event_status', 1)
+                ->where('events.dept_id', 0)
                 ->orderByDesc('events.end_date')
                 ->get();
 
